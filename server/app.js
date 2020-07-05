f95e60e284e69d312aa160047747f301501778b1
Anant Verma <anantverma311@gmail.com>
2020-07-05 09:02:19 +0530

    post data error not resolved and get req successfull

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 5000;
+
+app.get("/backend", (req, res) => {
+  const members = [
+    { id: 1, fname: "geetansh", lname: "monga" },
+    { id: 2, fname: "Agam", lname: "makhija" },
+    { id: 3, fname: "hemu", lname: "sharma" },
+    { id: 4, fname: "kartik", lname: "sharma" },
+  ];
+  res.send(members);
+});
+
+app.post("/backend" , (req ,res) => {
+  const data = req.body.name;
+  console.log(data);
+   res.send(console.log(data));
+})
+
+app.listen(port, () => {
+  console.log("server started at localhost:" + port);
+});
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "./Container";
 import PostData from "../postDataModel";
+import axios from "axios";
 
-function App() {
+function App(props) {
+  const [members, setMembers] = useState([]);
+  const [ member , setMember] = useState({ name: "" });
+
+  function changeHandler(event) {
+    const parada = setMember({ [event.target.name]: event.target.value });
+    console.log(parada);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const user = {
+      name: member,
+    };
+
+    axios
+      .post("/backend", user)
+      .then((response) => {
+        console.log("hello");
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    async function backend() {
+      axios
+        .get("/backend")
+        .then((result) => {
+          const resultData = result;
+          return setMembers(resultData.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    backend();
+  });
+
   return PostData.map((data) => {
     return (
-      <div>
+      <div key={data.id}>
         <Container
           URL={data.profileImage}
           profileName={data.userName}
           postContent={data.postContent}
           postImage={data.postImage}
         />
+        <div className="backendContainer">
+          <ul>
+            {members.map((member) => {
+              return (
+                <li key={member.id}>{member.fname + " " + member.lname}</li>
+              );
+            })}
+          </ul>
+          <form onSubmit={submitHandler}>
+            <input type="text" name="name" onChange={changeHandler}></input>
+            <button type="submit">send</button>
+          </form>
+        </div>
       </div>
     );
   });
